a3dc0ed7a44297f562f2fd7e3cf110e2702a4f79
ethanyambao <jann.yambao19@my.stjohns.edu>
2026-01-29 01:39:11 -0500

    feat: add database config and connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.db.session import engine
 
 app = FastAPI(title="NBA Analytics API")
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/db-health")
+def db_health():
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+    return {"db" : "ok"}
+    
